59995b1da478f0d11e08640ceec75dc4df8f2f27
toporovv <top.vetal@yandex.ru>
2021-07-15 23:03:28 +0300

    added users rest api

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
