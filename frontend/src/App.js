32bd283db861dac94e09efe4a79cda625c292509
toporovv <top.vetal@yandex.ru>
2021-07-20 23:39:14 +0300

    added axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './components/users.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {'users': []};
+    };
+
+    componentDidMount() {
+        this.setState({'users': users})
+    }
+
+    render() {
+        return (<div>
+            <UserList users={this.state.users} />
+        </div>)
+    };
+
+};
 
 export default App;
