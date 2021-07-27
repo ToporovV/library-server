2d9453c1e8eaf5c99b351563701fb69b66e37533
toporovv <top.vetal@yandex.ru>
2021-07-27 23:42:04 +0300

    added pagination and filter to project viewset

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
+from django_filters import rest_framework as filters
 
 from todo.models import Project, ToDo
 from todo.serializers import ProjectSerializer, ToDoSerializer
 
 
+class ProjectFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr='contains')
+
+    class Meta:
+        model = Project
+        fields = ['name']
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class ToDoViewSet(ModelViewSet):
     serializer_class = ToDoSerializer
     queryset = ToDo.objects.all()
-
